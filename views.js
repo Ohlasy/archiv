c04b5147739a01bf869a4d3d9f27316a838e6faa
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2015-12-22 09:05:10 +0100

    Přidal jsem datum vydání článků.

--- views.js
+++ views.js
@@ -1,9 +1,22 @@
+function parseJekyllDate(dateString) {
+    var JSCompatibleDateString = dateString.replace(" +0000", "").replace(" ", "T")
+    return new Date(JSCompatibleDateString)
+}
+
+function renderDate(date) {
+    var monthNames = ["ledna", "února", "března", "dubna", "května",
+        "června", "července", "srpna", "září", "října", "listopadu", "prosince"]
+    return date.getUTCDate() + ". " + monthNames[date.getUTCMonth()] + " " + date.getUTCFullYear()
+}
+
 function renderArticlePreview(article) {
     var link = $('<a/>', { 'href': 'http://ohlasy.info' + article.relativeURL })
-    var div = $('<div/>', {'class': 'article'})
+    var div = $('<div/>', {'class': 'article-preview'})
+    var dateStamp = renderDate(parseJekyllDate(article.pubDate))
     link.append($('<h2/>').html(article.title))
+    link.append($('<p/>', {'class': 'article-perex'}).html(article.perex))
+    link.append($('<p/>', {'class': 'article-meta text-muted'}).html(article.author + " / " + dateStamp))
     div.append(link)
-    div.append($('<p/>').html(article.perex))
     return div
 }
 
