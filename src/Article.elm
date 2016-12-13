8fe739641c5527be41a271639bf758be1bdd54fd
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2016-12-13 16:40:11 +0100

    First Elm code: download, parse and display the JSON article list

---
+++ src/Article.elm
@@ -0,0 +1,41 @@
+module Article exposing (Article, articleDecoder, articleListDecoder)
+
+import Json.Decode exposing (..)
+import Json.Decode.Pipeline exposing (..)
+import Date exposing (Date, fromString)
+import String.Extra exposing (replace)
+
+type alias Article = {
+    relativeURL: String,
+    title: String,
+    author: String,
+    category: Maybe String,
+    coverPhotoURL: Maybe String,
+    serialID: Maybe String,
+    pubDate: Date
+}
+
+articleDecoder : Decoder Article
+articleDecoder =
+    decode Article
+        |> required "relativeURL" string
+        |> required "title" string
+        |> required "author" string
+        |> required "category" (nullable string)
+        |> required "cover-photo" (nullable string)
+        |> required "serial" (nullable string)
+        |> required "pubDate" pubDateDecoder
+
+articleListDecoder : Decoder (List Article)
+articleListDecoder = list articleDecoder
+
+pubDateDecoder : Decoder Date
+pubDateDecoder =
+    string |> andThen (\s ->
+        case Date.fromString (convertDateToISO8601 s) of
+            Err e -> fail e
+            Ok d -> succeed d
+        )
+
+convertDateToISO8601 : String -> String
+convertDateToISO8601 = replace " " "T" << replace " +0000" ""
