22e82519d53f66a153a5c469bcd7d2bcab5e3b3c
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2017-01-16 11:02:10 +0100

    Trivial syntax changes

--- src/State.elm
+++ src/State.elm
@@ -22,33 +22,28 @@ type Msg =
 -- UPDATE
 
 update : Msg -> Model -> (Model, Cmd Msg)
-update msg model =
-    case msg of
-        ParseArticles result ->
-            case result of
-                Ok s ->
-                    case decodeString articleListDecoder s of
-                        Ok articles ->
-                            (Displaying articles defaultFilters Dict.empty, Cmd.none)
-                        Err e ->
-                            (Failed (toString e), Cmd.none)
-                Err e ->
-                    (Failed (toString e), Cmd.none)
-        UpdateFilterValue f val ->
-            case model of
-                Displaying articles filters settings ->
-                    let updatedSettings = case val of
-                        Just val -> Dict.insert f.name val settings
-                        Nothing -> Dict.remove f.name settings
-                    in (Displaying articles filters updatedSettings, Cmd.none)
-                _ ->
-                    (Failed "Invalid state", Cmd.none)
-        RemoveAllFilters ->
-            case model of
-                Displaying articles filters settings ->
-                    (Displaying articles filters Dict.empty, Cmd.none)
-                _ ->
-                    (Failed "Invalid state", Cmd.none)
+update msg model = case msg of
+    ParseArticles result -> case result of
+        Ok s -> case decodeString articleListDecoder s of
+            Ok articles ->
+                (Displaying articles defaultFilters Dict.empty, Cmd.none)
+            Err e ->
+                (Failed (toString e), Cmd.none)
+        Err e ->
+            (Failed (toString e), Cmd.none)
+    UpdateFilterValue f val -> case model of
+        Displaying articles filters settings ->
+            let updatedSettings = case val of
+                Just val -> Dict.insert f.name val settings
+                Nothing -> Dict.remove f.name settings
+            in (Displaying articles filters updatedSettings, Cmd.none)
+        _ ->
+            (Failed "Invalid state", Cmd.none)
+    RemoveAllFilters -> case model of
+        Displaying articles filters settings ->
+            (Displaying articles filters Dict.empty, Cmd.none)
+        _ ->
+            (Failed "Invalid state", Cmd.none)
 
 downloadArticles : Http.Request String
 downloadArticles = Http.getString "http://www.ohlasy.info/api/articles.js"
