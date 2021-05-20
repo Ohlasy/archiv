3d40b89bc9be525138aa1411def9da9880499354
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2021-05-20 11:41:33 +0200

    Fix article API path

--- src/State.elm
+++ src/State.elm
@@ -109,7 +109,7 @@ buildSearchUrl query =
 downloadArticles : Cmd Msg
 downloadArticles =
     Http.get
-        { url = "https://www.ohlasy.info/static-api/articles.js"
+        { url = "https://ohlasy.info/static-api/articles.js"
         , expect = Http.expectString ParseArticles
         }
 
