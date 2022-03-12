75cbe007f065f8c5df788c8cbdf365642b9c8f46
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-03-12 19:18:15 +0100

    Update filter logging format

--- components/MainPage.tsx
+++ components/MainPage.tsx
@@ -40,7 +40,7 @@ const MainPage: React.FC<Props> = (props) => {
       const newSettings = { ...settings, [id]: newValue };
       setSettings(newSettings);
       onSettingsChange(newSettings);
-      trackEvent(`filter`, { props: { id, value: newValue } });
+      trackEvent(`Filter`, { props: { value: `${id}:${newValue}` } });
     } else {
       const { [id]: _, ...newSettings } = settings;
       setSettings(newSettings);
